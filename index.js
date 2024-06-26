72c0ac91df74f6ce5cae2cf27b16cf0f15db47af
anywhereeanytime <whaatrudoing@gmail.com>
2024-06-26 19:52:27 +0700

    added local storage

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ let inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
 let myLeads = [];
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 buttonEl.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
   renderLeads();
 });
 
